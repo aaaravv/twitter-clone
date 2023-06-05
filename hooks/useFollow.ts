3d1c072901aefaeaaae80058d0160c2e71bdb977
aaaravv <aaravmishra619@gmail.com>
2023-06-05 21:34:15 +0530

    feat: add follow & unFollow functionality

---
+++ hooks/useFollow.ts
@@ -0,0 +1,66 @@
+import axios from "axios";
+import { useCallback, useMemo } from "react";
+import { toast } from "react-hot-toast";
+import useCurrentUser from "./useCurrentUser";
+import useLoginModal from "./useLoginModal";
+import useUser from "./useUser";
+
+const useFollow = (userId: string) => {
+  const { currentUser, mutate: mutateCurrentUser } = useCurrentUser();
+  const { data: fetchedUser, mutate: mutateFetchedUser } = useUser(userId);
+
+  const loginModal = useLoginModal();
+
+  const isFollowing = useMemo(() => {
+    return currentUser.followingIds.includes(userId);
+  }, [userId, currentUser?.followingIds]);
+
+  const toggleFollow = useCallback(async () => {
+    if (!currentUser) {
+      return loginModal.onOpen();
+    }
+
+    try {
+      let request;
+
+      if (isFollowing) {
+        request = () =>
+          axios.delete(`/api/follow`, {
+            params: {
+              userId,
+            },
+          });
+      } else {
+        request = () =>
+          axios.post(`/api/follow`, {
+            userId,
+          });
+      }
+
+      await request();
+
+      mutateCurrentUser();
+      mutateFetchedUser();
+
+      toast.success("Success");
+    } catch (error) {
+      toast.error("Something went wrong");
+      console.error(error);
+    }
+  }, [
+    currentUser,
+    fetchedUser,
+    isFollowing,
+    userId,
+    mutateCurrentUser,
+    mutateFetchedUser,
+    loginModal,
+  ]);
+
+  return {
+    isFollowing,
+    toggleFollow,
+  };
+};
+
+export default useFollow;
